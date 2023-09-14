9d7aee6ffd3077935796183fa1c0762c3e9acd6f
Graham Fujisawa <sfrrki@gmail.com>
2023-09-14 10:40:43 -0700

    add .env and .env example

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { NewsProvider } from "./NewsContext";
+import { Lobby } from "./Lobby";
+import "./index.module.css";
+import News from "./[news]";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const router = useRouter();
+  const { query } = router;
+  
+  let pageComponent;
+  if (query.page === "News") {
+    // pageComponent = <News newsCityInput={cityInput} newsResult={result} />;
+    pageComponent = <News />;
+  } else {
+    // pageComponent = <Lobby cityInput={cityInput} setCityInput={setCityInput} result={result} setResult={setResult} />;
+    pageComponent = <Lobby />;
+  }
 
   return (
     <>
+    <NewsProvider>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Head>
+          <title>Local News GPT</title>
+          <link rel="icon" href="/dog.png" />
+        </Head>
+        {pageComponent}
+        {/* {result && result[0].title} */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </NewsProvider>
+
     </>
   )
 }
