b260d89b832baad6c03ef297e81cf2ab40c05534
Graham Fujisawa <sfrrki@gmail.com>
2023-11-13 13:54:57 -0800

    update config to add build: outDir so render can use build files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This will output the build to the "dist" folder in the root directory
+  }
 })
-
-module.exports = {
-  publicPath: '/dist/'
-}
